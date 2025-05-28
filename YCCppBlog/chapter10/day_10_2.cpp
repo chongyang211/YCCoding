6b63a93fb6f2870147fbb97062746d63f2517d55
yangchong <yangchong211@163.com>
2025-05-28 18:56:43 +0800

    设计一个圆类，求圆的周长

--- YCCppBlog/chapter10/day_10_2.cpp
+++ YCCppBlog/chapter10/day_10_2.cpp
@@ -19,13 +19,13 @@ public:
     }
 };
 
-void test2_2(){
+void test2_1(){
     class Student yc1;  //正确
     Student yc2;  //同样正确
     Student yc3[100];   //创建对象数组
 }
 
-void test2_3() {
+void test2_2() {
     Student stu;
     stu.name = "打工充";
     stu.age = 30;
@@ -39,7 +39,7 @@ void test2_3() {
     student.say();
 }
 
-void test2_4() {
+void test2_3() {
     Student stu;
     Student *pStuCopy = &stu;
 
@@ -49,6 +49,31 @@ void test2_4() {
     pStu->score = 92.5f;
     pStu->say();
     delete pStu;  //删除对象
+}
+
+const double PI = 3.14;
+//封装一个圆类，求圆的周长
+//class代表设计一个类，后面跟着的是类名
+class Circle {
+public:     //访问权限  公共的权限
+    //属性
+    int r;
+    //行为
+    //获取到圆的周长
+    double calculate() {
+        //获取圆的周长
+        return 2*PI*r;
+    }
+};
+
+void test2_4() {
+    //通过圆类，创建圆的对象
+    // c1就是一个具体的圆
+    Circle c1;
+    c1.r = 10; //给圆对象的半径 进行赋值操作
+    //2 * pi * 10 = = 62.8
+    cout << "圆的周长为： " << c1.calculate() << endl;
+
 }
 
 int main() {
