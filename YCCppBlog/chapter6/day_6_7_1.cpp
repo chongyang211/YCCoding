6290e28b13c3d753b8077f555ad3aa67d94df85f
yangchong <yangchong211@163.com>
2025-06-13 10:43:50 +0800

    函数占位参数

---
+++ YCCppBlog/chapter6/day_6_7_1.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 杨充 on 2025/4/28.
+//
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    return 0;
+}
---
+++ YCCppBlog/chapter6/day_6_7_2.cpp
@@ -0,0 +1,16 @@
+//
+// Created by 杨充 on 2025/4/28.
+//
+#include<iostream>
+using namespace std;
+
+
+//函数占位参数 ，占位参数也可以有默认参数
+void func(int a, int) {
+    cout << "this is func" << endl;
+}
+
+int main() {
+    func(10,10); //占位参数必须填补
+    return 0;
+}
