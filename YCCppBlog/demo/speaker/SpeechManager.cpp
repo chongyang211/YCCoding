434d581d9a43520959e186706706904625e682aa
yangchong <yangchong211@163.com>
2025-06-28 18:31:05 +0800

    提供功能接口

---
+++ YCCppBlog/demo/speaker/SpeechManager.cpp
@@ -0,0 +1,71 @@
+//
+// Created by 杨充 on 2025/6/28.
+//
+
+#include "SpeechManager.h"
+
+SpeechManager::SpeechManager(){
+
+}
+
+SpeechManager::~SpeechManager(){
+
+}
+
+void clearScreen() {
+    std::cout << "\033[2J\033[1;1H"; // 使用 ANSI Escape Codes 清空屏幕
+    std::cout << "清空屏幕" << std::endl;
+}
+
+void pause() {
+    //替代 system("pause")
+    //system("pause");
+    std::cout << "点击enter键，暂停";
+    std::cin.get(); // 等待用户输入字符
+}
+
+//暂停和清空屏幕
+void pauseAndCls() {
+    pause();    //暂停
+    clearScreen(); // 清空屏幕
+}
+
+int main() {
+    SpeechManager sm;
+    int choice = 0; //用来存储用户的选项
+    while (true){
+        sm.showMenu();
+        cout << "请输入您的选择： " << endl;
+        cin >> choice; // 接受用户的选项
+        switch (choice){
+            case 1:  //开始比赛
+                break;
+            case 2:  //查看记录
+                break;
+            case 3:  //清空记录
+                break;
+            case 0:  //退出系统
+                break;
+            default:
+                system("cls"); //清屏
+            break;
+        }
+    }
+    pause();
+    return 0;
+}
+
+void SpeechManager::showMenu() {
+    cout << "********************************************" << endl;
+    cout << "*************  欢迎参加演讲比赛 ************" << endl;
+    cout << "*************  1.开始演讲比赛  *************" << endl;
+    cout << "*************  2.查看往届记录  *************" << endl;
+    cout << "*************  3.清空比赛记录  *************" << endl;
+    cout << "*************  0.退出比赛程序  *************" << endl;
+    cout << "********************************************" << endl;
+    cout << endl;
+}
+
+
+
+
---
+++ YCCppBlog/demo/speaker/SpeechManager.h
@@ -0,0 +1,25 @@
+//
+// Created by 杨充 on 2025/6/28.
+//
+#pragma once
+#include<iostream>
+using namespace std;
+
+#ifndef SPEECHMANAGER_H
+#define SPEECHMANAGER_H
+
+
+//演讲管理类
+class SpeechManager {
+public:
+    //构造函数
+    SpeechManager();
+    //析构函数
+    ~SpeechManager();
+    //展示菜单
+    void showMenu();
+};
+
+
+
+#endif //SPEECHMANAGER_H
