1dd978fd078def4bbb30021f94bbe2f701046daa
yangchong <yangchong211@163.com>
2025-06-30 16:04:13 +0800

    测试退出功能

--- YCCppBlog/demo/school/ComputerManager.cpp
+++ YCCppBlog/demo/school/ComputerManager.cpp
@@ -19,32 +19,45 @@ void pause() {
 
 //暂停和清空屏幕
 void pauseAndCls() {
-    pause();    //暂停
+    pause(); //暂停
     clearScreen(); // 清空屏幕
 }
 
 int main() {
-    cout << "======================  欢迎来到传智播客机房预约系统  =====================" << endl;
-    cout << endl << "请输入您的身份" << endl;
-    cout << "\t\t -------------------------------\n";
-    cout << "\t\t|                               |\n";
-    cout << "\t\t|          1.学生代表           |\n";
-    cout << "\t\t|                               |\n";
-    cout << "\t\t|          2.老    师           |\n";
-    cout << "\t\t|                               |\n";
-    cout << "\t\t|          3.管 理 员           |\n";
-    cout << "\t\t|                               |\n";
-    cout << "\t\t|          0.退    出           |\n";
-    cout << "\t\t|                               |\n";
-    cout << "\t\t -------------------------------\n";
-    cout << "输入您的选择: ";
+    int select = 0;
+    while (true) {
+        cout << "======================  欢迎来到传智播客机房预约系统  =====================" << endl;
+        cout << endl << "请输入您的身份" << endl;
+        cout << "\t\t -------------------------------\n";
+        cout << "\t\t|                               |\n";
+        cout << "\t\t|          1.学生代表           |\n";
+        cout << "\t\t|                               |\n";
+        cout << "\t\t|          2.老    师           |\n";
+        cout << "\t\t|                               |\n";
+        cout << "\t\t|          3.管 理 员           |\n";
+        cout << "\t\t|                               |\n";
+        cout << "\t\t|          0.退    出           |\n";
+        cout << "\t\t|                               |\n";
+        cout << "\t\t -------------------------------\n";
+        cout << "输入您的选择: ";
+        cin >> select; //接受用户选择
+        switch (select) {
+            case 1: //学生身份
+                break;
+            case 2: //老师身份
+                break;
+            case 3: //管理员身份
+                break;
+            case 0: //退出系统
+                cout << "欢迎下一次使用" << endl;
+                pause();
+                break;
+            default:
+                cout << "输入有误，请重新选择！" << endl;
+                clearScreen();
+                break;
+        }
+    }
     pause();
     return 0;
 }
-
-
-
-
-
-
-
