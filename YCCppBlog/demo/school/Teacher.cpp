12506e11facfff7496bd9fe7b7e3a78f4902e880
yangchong <yangchong211@163.com>
2025-06-30 16:18:50 +0800

    老师类分析和创建

---
+++ YCCppBlog/demo/school/Teacher.cpp
@@ -0,0 +1,27 @@
+//
+// Created by 杨充 on 2025/6/30.
+//
+
+#include "Teacher.h"
+
+//默认构造
+Teacher::Teacher() {
+}
+
+//有参构造 (职工编号，姓名，密码)
+Teacher::Teacher(int empId, string name, string pwd) {
+}
+
+//菜单界面
+void Teacher::operMenu() {
+}
+
+//查看所有预约
+void Teacher::showAllOrder() {
+}
+
+//审核预约
+void Teacher::validOrder() {
+}
+
+
