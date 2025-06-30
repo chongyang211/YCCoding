a9156d5cd95126b70098c23879ee09dffe232614
yangchong <yangchong211@163.com>
2025-06-30 16:21:19 +0800

    管理员分析和创建

---
+++ YCCppBlog/demo/school/Manager.h
@@ -0,0 +1,33 @@
+//
+// Created by 杨充 on 2025/6/30.
+//
+#pragma once
+#include<iostream>
+using namespace std;
+#include "identity.h"
+
+#ifndef MANAGER_H
+#define MANAGER_H
+
+
+
+class Manager : Identity{
+    //默认构造
+    Manager();
+    //有参构造  管理员姓名，密码
+    Manager(string name, string pwd);
+    //选择菜单
+    virtual void operMenu();
+    //添加账号
+    void addPerson();
+    //查看账号
+    void showPerson();
+    //查看机房信息
+    void showComputer();
+    //清空预约记录
+    void cleanFile();
+};
+
+
+
+#endif //MANAGER_H
