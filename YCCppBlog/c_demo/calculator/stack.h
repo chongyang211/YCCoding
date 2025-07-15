a1ae628d6ad69824d6487c41034c20e333cd878e
yangchong <yangchong211@163.com>
2025-07-15 10:24:32 +0800

    栈数据结构

--- YCCppBlog/c_demo/calculator/stack.h
+++ YCCppBlog/c_demo/calculator/stack.h
@@ -5,4 +5,27 @@
 #ifndef STACK_H
 #define STACK_H
 
-#endif //STACK_H
+#define STACK_SIZE 128
+
+// 栈数据结构
+typedef struct {
+    double data[STACK_SIZE];
+    int top;
+} Stack;
+
+// 初始化栈
+void stack_init(Stack* s);
+
+// 压入栈
+bool stack_push(Stack* s, double value);
+
+// 弹出栈
+bool stack_pop(Stack* s, double* value);
+
+// 查看栈顶
+bool stack_peek(Stack* s, double* value);
+
+// 检查栈是否为空
+bool stack_is_empty(Stack* s);
+
+#endif // STACK_H
