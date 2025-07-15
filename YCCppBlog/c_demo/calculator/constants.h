b60eebf4754c86870226fdae9885383d2ab8006a
yangchong <yangchong211@163.com>
2025-07-15 10:25:20 +0800

    常量

--- YCCppBlog/c_demo/calculator/constants.h
+++ YCCppBlog/c_demo/calculator/constants.h
@@ -5,4 +5,15 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#endif //CONSTANTS_H
+typedef struct {
+    const char* name;
+    double value;
+} MathConstant;
+
+// 获取数学常量
+const MathConstant* constants_get(const char* name);
+
+// 初始化常量表
+void constants_init(void);
+
+#endif // CONSTANTS_H
