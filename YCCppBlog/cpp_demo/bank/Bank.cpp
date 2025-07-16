c22c964d7fc8986aa5d088fa996b9c9dc0d9563f
yangchong <yangchong211@163.com>
2025-07-17 07:55:30 +0800

    开户

--- YCCppBlog/cpp_demo/bank/Bank.cpp
+++ YCCppBlog/cpp_demo/bank/Bank.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "Bank.h"
+
+// 开户
+void Bank::createAccount(const std::string &accNumber, const std::string &accName, double initialBalance) {
+    Account newAccount(accNumber, accName, initialBalance);
+    accounts.push_back(newAccount);
+    std::cout << "开户成功！" << std::endl;
+}
--- YCCppBlog/cpp_demo/bank/Bank.h
+++ YCCppBlog/cpp_demo/bank/Bank.h
@@ -2,13 +2,20 @@
 // Created by 杨充 on 2025/7/16.
 //
 
+#include <string>
+#include <iostream>
+#include "Account.h"
 #ifndef BANK_H
 #define BANK_H
 
 
 
 class Bank {
-
+private:
+    std::vector<Account> accounts; // 所有账户
+public:
+    // 开户
+    void createAccount(const std::string& accNumber, const std::string& accName, double initialBalance);
 };
 
 
