6b5398d4565ff4474d0ca319935a1a99cd6a68b5
yangchong <yangchong211@163.com>
2025-08-03 17:05:09 +0800

    老师类分析和创建

--- YCCppBlog/cpp_demo/school/Teacher.cpp
+++ YCCppBlog/cpp_demo/school/Teacher.cpp
@@ -14,14 +14,17 @@ Teacher::Teacher(int empId, string name, string pwd) {
 
 //菜单界面
 void Teacher::openMenu() {
+
 }
 
 //查看所有预约
 void Teacher::showAllOrder() {
+
 }
 
 //审核预约
 void Teacher::validOrder() {
+
 }
 
 
--- YCCppBlog/cpp_demo/school/Teacher.h
+++ YCCppBlog/cpp_demo/school/Teacher.h
@@ -12,19 +12,14 @@ class Teacher : public Identity {
 public:
     //默认构造
     Teacher();
-
     //有参构造 (职工编号，姓名，密码)
     Teacher(int empId, string name, string pwd);
-
     //菜单界面
     virtual void openMenu();
-
     //查看所有预约
     void showAllOrder();
-
     //审核预约
     void validOrder();
-
     //教师编号
     int empId;
 };
