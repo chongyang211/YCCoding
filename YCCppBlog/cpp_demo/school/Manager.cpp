b89acba893380bc0ac70e22cee6e894e969dcbf8
yangchong <yangchong211@163.com>
2025-08-04 21:38:08 +0800

    initVector

--- YCCppBlog/cpp_demo/school/Manager.cpp
+++ YCCppBlog/cpp_demo/school/Manager.cpp
@@ -88,3 +88,33 @@ void Manager::showComputer() {
 void Manager::cleanFile() {
 }
 
+void Manager::initVector() {
+    //读取学生文件信息
+    ifstream ifs;
+    ifs.open(STUDENT_FILE, ios::in);
+    if (!ifs.is_open()) {
+        cout << "学生文件读取失败" << endl;
+        return;
+    }
+    vStu.clear();
+    Student s;
+    while (ifs >> s.id && ifs >> s.name &&  ifs >> s.pwd) {
+        vStu.push_back(s);
+    }
+    cout << "当前学生数量为： " << vStu.size() << endl;
+    ifs.close(); //学生初始化
+    //读取老师文件信息
+    ifs.open(TEACHER_FILE, ios::in);
+    if (!ifs.is_open()) {
+        cout << "老师文件读取失败" << endl;
+        return;
+    }
+    vTea.clear();
+    Teacher t;
+    while (ifs >> t.empId && ifs >> t.name &&  ifs >> t.pwd) {
+        vTea.push_back(t);
+    }
+    cout << "当前教师数量为： " << vTea.size() << endl;
+    ifs.close();
+}
+
